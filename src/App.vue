7e71822e92846b7843dc74ab79d88c021d5e77c5
Dan Traian Roman <roman.dan.traian@gmail.com>
2022-03-10 19:51:33 +0100

    refactor to vite

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+
+
+<template>
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<script setup>
+import HelloWorld from './components/FrontPage.vue'
+</script>
+
+<style>
+
+</style>
---
+++ src/components/FrontPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <h1 class="title">The Design Room</h1>
+  <p class="text">Content in the making</p>
+  <p class="text">Coming soon 2022</p>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
