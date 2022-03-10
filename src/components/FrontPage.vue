9e7d23b83ed7a03ade5a3f861b1c2d9a7eb88520
Dan Traian Roman <roman.dan.traian@gmail.com>
2022-03-10 20:25:03 +0100

    favicon and font

--- src/components/FrontPage.vue
+++ src/components/FrontPage.vue
@@ -2,6 +2,7 @@
 </script>
 
 <template>
+  <img alt="Brand logo" src="../assets/logo.png" />
   <h1 class="title">The Design Room</h1>
   <p class="text">Content in the making</p>
   <p class="text">Coming soon 2022</p>
